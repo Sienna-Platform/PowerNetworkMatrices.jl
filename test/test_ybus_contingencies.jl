f7c78619f2f3b667f639bcbd05eb864d5be13dcc
m-bossart <bossart.matt@gmail.com>
2026-04-06 16:52:01 -0600

    add back needed modification

--- test/test_ybus_contingencies.jl
+++ test/test_ybus_contingencies.jl
@@ -16,6 +16,7 @@
         # Reference: disable branch and rebuild
         set_available!(branch, false)
         ybus_ref = Ybus(sys)
+        set_available!(branch, true)
 
         @test isapprox(modified_data, ybus_ref.data, atol = 1e-4)
     end
